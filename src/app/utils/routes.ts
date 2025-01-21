530b02ee921fd315007e976678e1806cf2084445
Chris Matzenbach <chris.matzenbach@omnisci.com>
2025-01-20 18:02:40 -0600

    Util function for advocate route, include pagination, page size, and search term

---
+++ src/app/utils/routes.ts
@@ -0,0 +1,26 @@
+import { GetAdvocatesResponse } from "../types/getAdvocateResponse";
+
+export const fetchAdvocates = async (
+  page: number = 1,
+  limit: number = 20,
+  search: string = ""
+): Promise<GetAdvocatesResponse> => {
+  console.log(page, limit, search);
+  const params = new URLSearchParams({
+    page: page.toString(),
+    limit: limit.toString(),
+  });
+
+  if (search && search !== "") {
+    params.append("search", search);
+  }
+  console.log(params.toString());
+
+  const response = await fetch(`/api/advocates?${params.toString()}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch advocates, is postgres running?");
+  }
+
+  const data: GetAdvocatesResponse = await response.json();
+  return data;
+};
